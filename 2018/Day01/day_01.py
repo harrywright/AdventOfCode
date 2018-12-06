49ae20189d5b354a3f06fb5bd894a9aa8cc85270
Harry Wright <harry@harrywright.net>
2018-12-06 21:03:18 +0000

    Day 1, Part 2.

--- 2018/Day01/day_01.py
+++ 2018/Day01/day_01.py
@@ -1,5 +1,16 @@
+import itertools
+
 changes = [int(frequency) for frequency in open("input.txt").readlines()]
 
 print(sum(changes))
 
+
+
+freq = 0
+seen = {0}
+for num in itertools.cycle(changes):
+    freq += num
+    if freq in seen:
+        print(freq); break
+    seen.add(freq)
 
