152b0316e87ad82e23b8adcb7af058bb6ed832db
Harry Wright <harrywright@users.noreply.github.com>
2018-12-21 23:30:33 +0000

    Day 4, WIP

--- 2018/Day04/day_04.py
+++ 2018/Day04/day_04.py
@@ -1,6 +1,45 @@
 logs = [line.strip() for line in open("input.txt", 'r').readlines()]
 
-print(logs)
+logs = sorted(logs)
 
-print("Testing from ipad")
+guard = None
+date = None
+first = True
+
+for log in logs:
+    entry = log.split()
+    
+    if entry[3][0] == '#':
+        if not first:
+            state = '.' * 60
+            flag = True
+            for i in sleep:
+                if flag:
+                    state = state[:i] + '#'  * (60 - i)
+                else:
+                    state = state[:i] + '.' * (60 - i)
+                flag = not flag
+            print(date, guard, state)
+        guard = entry[3][1:]
+        sleep = []
+        first = False
+        continue
+    
+    date = entry[0][1:]
+    minute = int(entry[1][3:5])
+    
+    if entry[2] in ('falls','wakes'):
+        sleep.append(minute)
+    
+    #print(date, guard, sleep, log)
+
+state = '.' * 60
+flag = True
+for i in sleep:
+    if flag:
+        state = state[:i] + '#'  * (60 - i)
+    else:
+        state = state[:i] + '.' * (60 - i)
+    flag = not flag
+print(date, guard, state)
 
